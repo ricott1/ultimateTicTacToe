2fcc84a9d113296bdaf64fcda5ffe4fae080db01
ewf-updater <ewf-updater@ewf.org>
2018-07-04 12:17:28 +0200

    ultimate!

--- main.py
+++ main.py
@@ -2,26 +2,95 @@ import numpy as np
 from player import Guadalupe, Elora
 import time
 
+class UltimateBoard(object):
+    """docstring for UltimateBoard"""
+    def __init__(self):
+        self.state = np.empty( (3,3), dtype=object)
+        w, h = self.shape
+        for x in xrange(w):
+            for y in xrange(h):
+                self.state[x, y] = Board()
+        self.active_board = "any"
+    
+    def print_state(self):
+        w, h = self.shape
+
+        #check if someone win to show it in the print state?
+        for x in xrange(w):
+            all_lines = [self.state[x, y].print_list() for y in xrange(h)]
+            for i in xrange(len(all_lines[0])):
+                row = [l[i] for l in all_lines]
+                print " " + "  \"  ".join(row) + " "
+            if x < w-1:
+                print "              \"               \"             "
+                print "============================================="
+                print "              \"               \"             "
+        
+    def update(self, move, turn):
+        x, y, i, j = move
+        self.active_board = self.state[x, y]
+        self.active_board.update((i, j), turn)
+        next_board = self.state[i, j]
+        if next_board.closed:
+            self.active_board = "any"
+        else:
+            self.active_board = next_board 
+            #need last board to check winner
+
+    def has_winner(self, move):
+        x, y, i, j = move
+
+        if self.state[0, y].closed == self.state[1, y].closed == self.state[2, y].closed in ("x", "o"):
+            return True
+
+        if self.state[x, 0].closed == self.state[x, 1].closed == self.state[x, 2].closed in ("x", "o"):
+            return True
+
+        if x == y and self.state[0, 0].closed == self.state[1, 1].closed == self.state[2, 2].closed in ("x", "o"):
+            return True
+
+        if x + y == 2 and self.state[0, 2].closed == self.state[1, 1].closed == self.state[2, 0].closed in ("x", "o"):
+            return True
+
+        return False
+
+    def is_full(self):
+        w, h = self.shape
+        for x in xrange(w):
+            for y in xrange(h):
+                if not self.state[x, y].closed:
+                    return False
+        return True
+
+    @property
+    def shape(self):
+        return self.state.shape
+
 class Board(object):
     def __init__(self):
         self.state = np.zeros((3,3))
+        self.closed = False
 
     def update(self, move, turn):
         self.state[move[0], move[1]] = 1. - 2 * (turn%2)
+        if self.has_winner(move):
+            self.closed = ["x", "o"][turn%2]
+        if self.is_full():
+            self.closed = "draw"
 
     def has_winner(self, move):
         x, y = move
 
-        if self.state[0][y] == self.state[1][y] == self.state[2][y]:
+        if self.state[0, y] == self.state[1, y] == self.state[2, y]:
             return True
 
-        if self.state[x][0] == self.state[x][1] == self.state[x][2]:
+        if self.state[x, 0] == self.state[x, 1] == self.state[x, 2]:
             return True
 
-        if x == y and self.state[0][0] == self.state[1][1] == self.state[2][2]:
+        if x == y and self.state[0, 0] == self.state[1, 1] == self.state[2, 2]:
             return True
 
-        if x + y == 2 and self.state[0][2] == self.state[1][1] == self.state[2][0]:
+        if x + y == 2 and self.state[0, 2] == self.state[1, 1] == self.state[2, 0]:
             return True
 
         return False
@@ -30,12 +99,36 @@ class Board(object):
         w, h = self.shape
         return not bool(len([1 for x in xrange(w) for y in xrange(h) if self.state[x, y] == 0]))
 
-    def print_state(self):
+    def print_list(self):
+        lines = []
         w, h = self.shape
-        for x in xrange(w):
-            print " " + " | ".join([[" ", "o", "x"][int(self.state[x, y])] for y in xrange(h)]) + " "
-            if x < w-1:
-                print "-----------"
+        if self.closed == "x":
+            lines = [r"  \     /  ",
+                     r"   \   /   ",
+                     r"    > <    ",
+                     r"   /   \   ",
+                     r"  /     \  "]
+        elif self.closed == "o":
+            lines = [r"   _____   ",
+                     r"  /     \  ",
+                     r"  |     |  ",
+                     r"  \     /  ",
+                     r"   -----   "]
+        elif self.closed == "draw":
+            for x in xrange(w):
+                lines.append(" " + " | ".join(["d" for y in xrange(h)]) + " ")
+                if x < w-1:
+                    lines.append("-----------")
+        else:
+            for x in xrange(w):
+                lines.append(" " + " | ".join([[" ", "x", "o"][int(self.state[x, y])] for y in xrange(h)]) + " ")
+                if x < w-1:
+                    lines.append("-----------")
+        return lines
+
+    def print_state(self):
+        for line in self.print_list():
+            print line
 
     @property
     def shape(self):
@@ -46,17 +139,17 @@ class Board(object):
 class Game(object):
     """docstring for Game"""
     def __init__(self, board, player1, player2):
-        self.board = board
+        self.u_board = board
         self.player1 = player1
         self.player2 = player2
         self.turn = 0
 
     def next_move(self):
         if self.turn%2 == 0:
-            next_move = self.player1.move(self.board, self.turn)
+            next_move = self.player1.move(self.u_board, self.turn)
         elif self.turn%2 == 1:
-            next_move = self.player2.move(self.board, self.turn)
-        self.board.update(next_move, self.turn)
+            next_move = self.player2.move(self.u_board, self.turn)
+        self.u_board.update(next_move, self.turn)
         self.turn += 1
         return next_move
 
@@ -65,20 +158,22 @@ class Game(object):
         
 
 if __name__ == '__main__':
-    board = Board()
-    game = Game(board, Elora(), Guadalupe())
-
+    
+    ultimate_board = UltimateBoard()
+    ultimate_board.print_state()
+    game = Game(ultimate_board, Elora(),  Guadalupe())# Guadalupe())
     while True:
         active_player = game.active_player()
         print "turn {} - {} to move".format(game.turn, active_player)
         move = game.next_move()
         print "{} moves to {}. New board state:".format(active_player, move)
-        board.print_state()
-        winner = board.has_winner(move)
-        if board.is_full():
-            print "Draw"
-            break
-        elif winner:
+        ultimate_board.print_state()
+        winner = ultimate_board.has_winner(move)
+        
+        if winner:
             print active_player + " won"
             break
-        time.sleep(1)
+        elif ultimate_board.is_full():
+            print "Draw"
+            break
+        time.sleep(0.1)
--- player.py
+++ player.py
@@ -1,35 +1,54 @@
 import random 
+import numpy as np
 
 class Guadalupe(object):
-    """docstring for Player"""
+    """Guadalupe plays a random legal move"""
     def __init__(self):
         self.name = "Guadalupe"
 
     def move(self, board, turn):
-        w, h = board.shape
-        move_set = [(x, y) for x in xrange(w) for y in xrange(h) if board.state[x, y] == 0 ]
-        print move_set
-
-        move = random.sample(move_set, 1)[0]
-        return move
+        w, h = board.state[0,0].shape
+        if board.active_board != "any":
+            x, y = np.where(board.state == board.active_board)
+            x = x[0]
+            y= y[0]
+        else:
+            x, y = random.sample([(i, j) for i in xrange(w) for j in xrange(h) if not board.state[i, j].closed], 1)[0]
+        print x, y
+        move_set = [(x, y, i, j) for i in xrange(w) for j in xrange(h) if board.state[x, y].state[i, j] == 0 ]
+        
+        if move_set:
+            return random.sample(move_set, 1)[0]
+        else:
+            return False
 
 
 class Elora(object):
-    """docstring for Player"""
+    """Elora uses a neural network to generate the best move. She is trained with genetic selection"""
     def __init__(self):
         self.name = "Elora"
         import brain
-        self.brain = brain.Brain([9, 6, 9])
+        self.brain = brain.Brain([81, 6, 9])
 
     def move(self, board, turn):
         w, h = board.shape
+        inputs = []
+        for x in xrange(w):
+            for y in xrange(h):
+                inputs.append(board.state[x, y].state.reshape(1, 9).tolist()[0])
+        inputs = [j for i in inputs for j in i]
+        if board.active_board != "any":
+            x, y = np.where(board.state == board.active_board)
+            x = x[0]
+            y= y[0]
+        else:
+            x, y = random.sample([(i, j) for i in xrange(w) for j in xrange(h) if not board.state[i, j].closed], 1)[0]
         
-        inputs = board.state.reshape(1,9).tolist()[0]
+        
         results = self.brain.propagate(inputs, 0)
-        all_moves = [(x, y) for x in xrange(w) for y in xrange(h)]
-        legal_moves = [(x, y) for x in xrange(w) for y in xrange(h) if board.state[x, y] == 0]
+        all_moves = [(x, y, i, j) for i in xrange(w) for j in xrange(h)]
+        legal_moves = [(x, y, i, j) for i in xrange(w) for j in xrange(h) if board.state[x, y].state[i, j] == 0]
         for i, r in enumerate(sorted(results, reverse = True)):
-            print r
             if all_moves[i] in legal_moves:
                 return all_moves[i]
         else:
